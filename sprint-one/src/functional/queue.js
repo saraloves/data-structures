82ca02371ae23fd8351103b4f5a2f146fded659f
Hack Reactor Students <sarabinns@gmail.com>
2013-11-13 15:51:13 -0800

    Queue working except queue specific behavior.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,13 +1,18 @@
 var makeQueue = function(){
   var instance = {};
+  var storage = {};
   var size = 0;
+
   // Implement the methods below
 
   instance.enqueue = function(value){
     size++;
+    storage[size] = value;
   };
 
   instance.dequeue = function(){
+    size && size--;
+    return storage[1];
   };
 
   instance.size = function(){
