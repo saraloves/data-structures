cd54833c52aa36081417b3ddd867c350fedb8acf
Hack Reactor Students <sarabinns@gmail.com>
2013-11-14 10:42:43 -0800

    Pseudoclassical stack and queue complete

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,24 @@
 var Queue = function() {
   // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  this.storage = {};
+  this.length = 0;
+
+  // Implement the methods below
 };
+
+Queue.prototype.enqueue = function(value){
+  this.length++;
+  this.storage[this.length] = value;
+};
+Queue.prototype.dequeue = function(){
+  var result = this.storage[1];
+  this.length && this.length--;
+  for (var i = 1; i <= this.length; i++) {
+    this.storage[i] = this.storage[i + 1];
+  }
+  return result;
+};
+Queue.prototype.size = function(){
+  return this.length;
+};
+
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,22 @@
 var Stack = function() {
   // Hey! Copy your code from src/prototypal/stack.js and paste it here
+  // Use an object with numeric keys to store values
+  this.storage = {};
+  this.length = 0; // Hint: set an initial value here
+
+  // Implement the methods below
 };
+
+Stack.prototype.push = function(value){
+  this.storage[this.length++] = value;
+};
+
+Stack.prototype.pop = function(){
+  this.length && this.length--;
+  return this.storage[this.length];
+};
+
+Stack.prototype.size = function(){
+  return this.length;
+};
+
