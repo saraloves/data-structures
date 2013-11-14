2c99f2a36fc895b801d3c54ab191ff1239b09f86
Hack Reactor Students <sarabinns@gmail.com>
2013-11-13 19:59:59 -0800

    Stack functional-shared working.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,5 +1,31 @@
 var makeStack = function() {
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+  // Hey! Copy your code from src/functional/stack.js and paste it
+  var instance = {};
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.length = 0; // Hint: set an initial value here
+
+  // Implement the methods below
+  instance.push = stackMethods.push;
+  instance.pop = stackMethods.pop;
+  instance.size = stackMethods.size;
+
+  return instance;
 };
 
-var stackMethods = {};
+
+var stackMethods = {
+  push: function(value){
+    this.storage[this.length++] = value;
+  },
+
+  pop: function(){
+    this.length && this.length--;
+    return this.storage[this.length];
+  },
+
+  size: function(){
+    return this.length;
+  }
+};
