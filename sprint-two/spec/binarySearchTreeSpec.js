b1df34be4b6ee32161da6f948692a10acde2c917
Hack Reactor Students <sarabinns@gmail.com>
2013-11-16 12:23:06 -0800

    Finished insert on binary search tree.

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -10,5 +10,22 @@ describe("binarySearchTree", function() {
     expect(binarySearchTree.contains).toEqual(jasmine.any(Function));
     expect(binarySearchTree.depthFirstLog).toEqual(jasmine.any(Function));
   });
+  it("should insert node into left node when value is smaller than root", function(){
+    binarySearchTree.value = 10;
+    binarySearchTree.insert(5);
+    expect(binarySearchTree.left.value).toEqual(5);
+  });
+  it("should insert node into right node when value is greater than root", function(){
+    binarySearchTree.value = 10;
+    binarySearchTree.insert(15);
+    expect(binarySearchTree.right.value).toEqual(15);
+  });
+  it("should insert node into a tree with depth", function(){
+    binarySearchTree.value = 10;
+    binarySearchTree.insert(15);
+    binarySearchTree.insert(5);
+    binarySearchTree.insert(2);
+    expect(binarySearchTree.left.left.value).toEqual(2);
+  });
   // add more tests here to test the functionality of binarySearchTree
 });
--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,2 +1,35 @@
 var makeBinarySearchTree = function(){
+  var tree = {};
+  tree.left = undefined;
+  tree.right = undefined;
+  tree.value = undefined;
+  tree.insert = function(value){
+    //if value is greater than tree.value, set value to tree.right, if tree.right === undefined. otherwise,
+    //compare value to tree.right
+    //keep going recursionally
+    //else if value is less than tree.value, set value to tree.left, if tree.left === undefined. otherwise,
+    //compare value to tree.left
+    //keep going recursionally
+    var node = makeBinarySearchTree();
+    node.value = value;
+    if (value > tree.value) {
+      if (tree.right === undefined) {
+        tree.right = node;
+      } else {
+        tree.right.insert(value);
+      }
+    } else if (value < tree.value) {
+      if (tree.left === undefined) {
+        tree.left = node;
+      } else {
+        tree.left.insert(value);
+      }
+    }
+  };
+  tree.contains = function(value){
+  };
+  tree.depthFirstLog = function(callback){
+
+  };
+  return tree;
 };
